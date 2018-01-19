6812ddd2b5bc9021dcdedfd3b18a84f34e2d6689
Diego Perez <diego@dsindigo.com>
2018-01-19 13:09:05 -0600

    Valida archivo no vacio resolve NA
    Se adiciono la prueba para validar que el archivo no venga vacio

--- CNS4_PRUEBASDiegoPerez/src/main/java/minessearcher/MinesSearcher.java
+++ CNS4_PRUEBASDiegoPerez/src/main/java/minessearcher/MinesSearcher.java
@@ -1,6 +1,24 @@
 package minessearcher;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class MinesSearcher {
 
+    public String[][] processMinesSearcher(String entryFile) {
+        String[][] finalMap = null;
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(entryFile));
+            String line;
+            while((line = in.readLine()) != null)
+            {
+                finalMap = new String[1][1];
+            }
+            in.close();
+            return finalMap;
+        } catch (Exception e) {
+            return finalMap;
+        }
+    }
 
 }
--- CNS4_PRUEBASDiegoPerez/src/main/java/minessearcher/MinesSearcherTest.java
+++ CNS4_PRUEBASDiegoPerez/src/main/java/minessearcher/MinesSearcherTest.java
@@ -1,6 +1,17 @@
 package minessearcher;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MinesSearcherTest {
 
+    MinesSearcher minesSearcher = new MinesSearcher();
+    
+    @Test
+    public void validateFileNotEmpty() {
+        String entryFile = "D:\\prueba1.txt";
+        String[][] finalMap = minesSearcher.processMinesSearcher(entryFile);
+        Assert.assertNotEquals(null, finalMap);
+    }
     
 }
